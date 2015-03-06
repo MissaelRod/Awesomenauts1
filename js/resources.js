20ef596ab13da6409478fb0e950a2a1282b82ebe
MissaelRodriguez <Blackphantom@gmail.com>
2015-03-06 13:59:41 -0800

    no message

--- js/resources.js
+++ js/resources.js
@@ -4,6 +4,9 @@ game.resources = [
      * {name: "example", type:"image", src: "data/img/example.png"},
      */
     {name: "background-tiles", type: "image", src: "data/img/background-tiles.png"},
+    {name: "meta-tiles", type:"image", src: "data/img/meta-tiles.png"},
+    {name: "player", type:"image", src: "data/img/orcSpear.png"},
+    
     /* Atlases 
      * @example
      * {name: "example_tps", type: "tps", src: "data/img/example_tps.json"},
@@ -14,14 +17,17 @@ game.resources = [
      * {name: "example01", type: "tmx", src: "data/map/example01.tmx"},
      * {name: "example01", type: "tmx", src: "data/map/example01.json"},
      */
+    {name: "level01", type:"tmx", src: "data/map/level01.tmx"}
+	
+        
+    /* Background music. 
+     * @example
+     * {name: "example_bgm", type: "audio", src: "data/bgm/"},
+     */	
 
-	/* Background music. 
-	 * @example
-	 * {name: "example_bgm", type: "audio", src: "data/bgm/"},
-	 */	
-
-	/* Sound effects. 
-	 * @example
-	 * {name: "example_sfx", type: "audio", src: "data/sfx/"}
-	 */
+    /* Sound effects. 
+     * @example
+     * {name: "example_sfx", type: "audio", src: "data/sfx/"}
+     */
 ];
+
